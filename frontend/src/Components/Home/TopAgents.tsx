19da0756c0cc1db4b993d2060e4a880dcbc20f6f
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-18 00:51:21 +0100

    Cooking

--- frontend/src/Components/Home/TopAgents.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TopAgents = () => {
-  return (
-    <div>TopAgents</div>
-  )
-}
-
-export default TopAgents
---
+++ frontend/src/Components/Index.ts
@@ -0,0 +1,13 @@
+import AgentCard from "./Agents/AgentCard";
+
+import CustomButton from "./Common/CustomButton";
+import Profile from "./Common/Profile";
+import PropertyCard from "./Common/PropertyCard";
+
+import PieChart from "./Charts/PieChart";
+import PropertyReferrals from "./Charts/PropertyReferrals";
+import TotalRevenue from "./Charts/TotalRevenue";
+
+import TopAgent from "./Home/TopAgent";
+
+export { AgentCard, CustomButton, PieChart, Profile, PropertyCard, PropertyReferrals, TopAgent, TotalRevenue}
