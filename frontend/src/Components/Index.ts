d199d7860a040158ada9b8199baab8e91b3d4408
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-19 23:47:48 +0100

    Tired but Cooking

--- frontend/src/Components/Index.ts
+++ frontend/src/Components/Index.ts
@@ -1,13 +1,15 @@
 import AgentCard from "./Agents/AgentCard";
 
-import CustomButton from "./Common/CustomButton";
-import Profile from "./Common/Profile";
-import PropertyCard from "./Common/PropertyCard";
-
 import PieChart from "./Charts/PieChart";
 import PropertyReferrals from "./Charts/PropertyReferrals";
 import TotalRevenue from "./Charts/TotalRevenue";
 
+import CustomButton from "./Common/CustomButton";
+import Form from "./Common/Form";
+import Profile from "./Common/Profile";
+import PropertyCard from "./Common/PropertyCard";
+
+
 import TopAgent from "./Home/TopAgent";
 
-export { AgentCard, CustomButton, PieChart, Profile, PropertyCard, PropertyReferrals, TopAgent, TotalRevenue}
+export { AgentCard, CustomButton, Form, PieChart, Profile, PropertyCard, PropertyReferrals, TopAgent, TotalRevenue}
