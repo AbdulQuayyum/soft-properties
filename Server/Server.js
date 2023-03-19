f4eaa054abc04608e1edc9b26fb1611092841b7e
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-20 00:47:05 +0100

    Tired but Cooking

--- Server/Server.js
+++ Server/Server.js
@@ -2,12 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import ConnectDB from "./Source/MongoDB/Connect"
-
-let PORT
-process.env.STATUS === "development"
-    ? (PORT = process.env.DEV_ENDPOINT_URL)
-    : (PORT = process.env.PROD_ENDPOINT_URL)
+import ConnectDB from "./Source/MongoDB/Connect.js"
 
 dotenv.config();
 
@@ -15,20 +10,30 @@ const app = express()
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (req, res) => {
-    res.send({ message: "Hello World!" });
+app.get('/', async (req, res) => {
+    res.status(200).json({
+        message: 'Heyy there, I am a Cat.',
+    });
 });
+
+let PORT;
+
+if (process.env.STATUS === 'development') {
+    PORT = process.env.DEV_ENDPOINT_URL;
+} else {
+    PORT = process.env.PROD_ENDPOINT_URL;
+}
+
 
 const StartServer = async () => {
     try {
-        ConnectDB(process.env.MONGODB_URL);
-
-        app.listen(PORT, () => {
-            console.log(`Server is in ${process.env.STATUS} mode, listening on port ${PORT}`)
-        })
+      ConnectDB(process.env.MONGODB_URL);
+      app.listen(PORT, () => {
+        console.log(`Server is in ${process.env.STATUS} mode, listening on port ${PORT}`);
+      });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-};
-
-StartServer()
+  };
+  
+  StartServer();
