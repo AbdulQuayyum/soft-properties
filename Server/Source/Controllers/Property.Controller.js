8bf4e6a3a17b8c8d6a9c477a48d0b4e9cb292e0c
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-20 01:04:05 +0100

    Tired but still Cooking

---
+++ Server/Source/Controllers/Property.Controller.js
@@ -0,0 +1,10 @@
+import Property from "../MongoDB/Models/Property";
+import User from "../MongoDB/Models/User";
+
+const CreateProperty = async (req, res) => { }
+const DeleteProperty = async (req, res) => { }
+const GetAllProperties = async (req, res) => { }
+const GetPropertyDetails = async (req, res) => { }
+const UpdateProperty = async (req, res) => { }
+
+export { CreateProperty, DeleteProperty, GetAllProperties, GetPropertyDetails, UpdateProperty }
---
+++ Server/Source/Controllers/User.Controller.js
@@ -0,0 +1,7 @@
+import User from "../MongoDB/Models/User";
+
+const CreateUser = async (req, res) => { }
+const GetAllUsers = async (req, res) => { }
+const GetUserInfoByID = async (req, res) => { }
+
+export { CreateUser, GetAllUsers, GetUserInfoByID }
