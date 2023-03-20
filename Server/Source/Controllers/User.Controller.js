6f070bf9da508e90e2d8ca350c473ec32eb52f31
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-20 02:31:27 +0100

    Tired but still Cooking

--- Server/Source/Controllers/User.Controller.js
+++ Server/Source/Controllers/User.Controller.js
@@ -1,7 +1,49 @@
 import User from "../MongoDB/Models/User.js";
 
-const CreateUser = async (req, res) => { }
-const GetAllUsers = async (req, res) => { }
-const GetUserInfoByID = async (req, res) => { }
+const CreateUser = async (req, res) => {
+    try {
+        const { name, email, avatar } = req.body;
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) return res.status(200).json(userExists);
+
+        const newUser = await User.create({
+            name,
+            email,
+            avatar,
+        });
+
+        res.status(200).json(newUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const GetAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).limit(req.query._end);
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const GetUserInfoByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findOne({ _id: id }).populate("allProperties");
+
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 
 export { CreateUser, GetAllUsers, GetUserInfoByID }
