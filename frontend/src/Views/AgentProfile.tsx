ee26023b73899d3de5f073c5aa58cfee40b6c124
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-21 23:36:25 +0100

    Tired and sleepy but still Cooking

--- frontend/src/Views/AgentProfile.tsx
+++ frontend/src/Views/AgentProfile.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useOne } from '@pankod/refine-core'
+import { useParams } from '@pankod/refine-react-router-v6'
+
+import { Profile } from 'Components/Index'
 
 const AgentProfile = () => {
+  const { id } = useParams()
+
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: id as string,
+  })
+
+  console.log(data)
+
+  const myProfile = data?.data ?? []
+
+  if (isLoading) return <div>loading...</div>
+  if (isError) return <div>error...</div>
+
   return (
-    <div>AgentProfile</div>
+    <Profile
+      type="Agent"
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties}
+    />
   )
 }
 
