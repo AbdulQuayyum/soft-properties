52d03785bc45f35dc9766268919279c79cdc8782
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-21 23:58:23 +0100

    Tired and sleepy but still Cooking

--- frontend/src/Views/EditProperty.tsx
+++ frontend/src/Views/EditProperty.tsx
@@ -1,8 +1,3 @@
-import { useState } from "react";
-import { useGetIdentity } from "@pankod/refine-core";
-import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
-import { Form } from "../Components/Index"
-
 const EditProperty = () => {
   return (
     <div>EditProperty</div>
