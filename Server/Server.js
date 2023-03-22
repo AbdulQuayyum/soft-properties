0bd0013f5a79b0dd5b1a9e97766e7c9aab163a70
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-22 02:39:36 +0100

    Tired and sleepy but still Cooking

--- Server/Server.js
+++ Server/Server.js
@@ -13,31 +13,32 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.get('/', async (req, res) => {
-    res.status(200).json({
-        message: 'Heyy there, I am a Cat.',
-    });
+  res.status(200).json({
+    message: 'Heyy there, I am a Cat.',
+  });
 });
 
-app.use("/api/v1/Properties", PropertyRouter )
+app.use("/api/v1/Properties", PropertyRouter)
 app.use("/api/v1/Users", UserRouter)
 
-let PORT;
+// let PORT;
 
-if (process.env.STATUS === 'development') {
-    PORT = process.env.DEV_ENDPOINT_URL;
-} else {
-    PORT = process.env.PROD_ENDPOINT_URL;
-}
+// if (process.env.STATUS === 'development') {
+//     PORT = process.env.DEV_ENDPOINT_URL;
+// } else {
+//     PORT = process.env.PROD_ENDPOINT_URL;
+// }
 
 const StartServer = async () => {
-    try {
-      ConnectDB(process.env.MONGODB_URL);
-      app.listen(PORT, () => {
-        console.log(`Server is in ${process.env.STATUS} mode, listening on port ${PORT}`);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  StartServer();
+  try {
+    ConnectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => {
+      console.log("Cooking")
+      // console.log(`Server is in ${process.env.STATUS} mode, listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+StartServer();
